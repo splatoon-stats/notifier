8a8611b664b2a3223ff1accab735a3e502cd071b
yukidaruma <yukidaruma@users.noreply.github.com>
2021-01-22 06:49:12 +0900

    fixup to feat(app:dev): Add support for Firebase Database Emulator

--- lib/firebase.dart
+++ lib/firebase.dart
@@ -5,9 +5,9 @@ export 'package:firebase_core/firebase_core.dart';
 export 'package:firebase_database/firebase_database.dart';
 
 FirebaseDatabase get database {
-  if (Config.DEV_FIREBASE_DATABASE_URL != null) {
-    return FirebaseDatabase(databaseURL: Config.DEV_FIREBASE_DATABASE_URL);
-  }
-
-  return FirebaseDatabase.instance;
+  return Config.DEV_FIREBASE_DATABASE_URL == null
+      ? FirebaseDatabase.instance
+      : FirebaseDatabase(
+          databaseURL: Config.DEV_FIREBASE_DATABASE_URL,
+        );
 }
